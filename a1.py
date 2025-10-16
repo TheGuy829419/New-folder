1309170329611bee86d0174e5128ac53bcb49c66
TheGuy829419 <nnennaibeneme7@gmail.com>
2025-10-16 15:55:12 -0600

    initial commit

---
+++ a1.py
@@ -0,0 +1,40 @@
+import cv2
+
+
+image = cv2.imread('Blox Fruits.png')
+
+cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Loaded Image', 200, 200)
+
+cv2.imshow('Loaded Image', image)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
+
+print(f"Image Dimensions: {image.shape}")
+
+
+import cv2
+
+image = cv2.imread('Blox Fruits.png')
+
+cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Loaded Image', 400, 400)
+
+cv2.imshow('Loaded Image', image)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
+
+print(f"Image Dimensions: {image.shape}")
+
+import cv2
+
+image = cv2.imread('Blox Fruits.png')
+
+cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Loaded Image', 600, 600)
+
+cv2.imshow('Loaded Image', image)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
+
+print(f"Image Dimensions: {image.shape}")
